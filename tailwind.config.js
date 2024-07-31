20687bf77ad672b0eb22eb31b79351946d6c132c
Tiago Matos <tiago@tiagomatos.com>
2024-08-01 09:21:17 +1000

    Demo criado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./**/*.{html,js}"],
   darkMode: 'class',
-  content: ['./src/**/*.html'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [],
 }
+
